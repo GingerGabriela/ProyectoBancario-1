cc81251ff7f5174ef6bd67e53a5fe3d8cf18b8b6
GingerGabriela <gnortiz25@gmail.com>
2020-04-27 23:45:01 +0200

    Documentacion de la clase Credito

--- Bancos/src/tarjetas/Credito.java
+++ Bancos/src/tarjetas/Credito.java
@@ -2,17 +2,41 @@ package tarjetas;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-
+/**
+ * 
+ * @author GingerGabriela
+ * @version v1.2020
+ * @since 28/04/2020
+ *
+ */
 public class Credito extends Tarjeta {
+	private static final double contImporte = 0.0;
+	/**
+	 * Atributo  mCredito
+	 */
 	protected double mCredito;
+	/**
+	 * Atributo lista de movimiento
+	 */
 	protected ArrayList<Movimiento> mMovimientos;
 
+	/**
+	 * Constructor con 5 par�metros
+	 * @param numero Numero de Credito
+	 * @param titular Titular de la Cuenta
+	 * @param fechaCaducidad Fecha de la caducidad
+	 * @param credito Credito
+	 */
 	public Credito(String numero, String titular, LocalDate fechaCaducidad, double credito) {
 		super(numero, titular, fechaCaducidad);
 		mCredito = credito;
 		mMovimientos = new ArrayList<Movimiento>();
 	}
 
+	/**
+	 * Metodo retirada en cajero automatico
+	 * @param x
+	 */
 	public void retirar(double x) throws Exception {
 		Movimiento m = new Movimiento();
 		m.setmConcepto("Retirada en cajero automático");
@@ -23,6 +47,10 @@ public class Credito extends Tarjeta {
 			throw new Exception("Crédito insuficiente");
 	}
 
+	/**
+	 * Metodo ingresar dinero en cuenta asociada
+	 * @param x 
+	 */
 	public void ingresar(double x) throws Exception {
 		Movimiento m = new Movimiento();
 		m.setmConcepto("Ingreso en cuenta asociada (cajero automático)");
@@ -31,6 +59,10 @@ public class Credito extends Tarjeta {
 		mCuentaAsociada.ingresar(x);
 	}
 
+	/**
+	 * Metodo para pagar en establecimeinto
+	 * @param datos datos de la persona
+	 */
 	public void pagoEnEstablecimiento(String datos, double x) throws Exception {
 		Movimiento m = new Movimiento();
 		m.setmConcepto("Compra a crédito en: " + datos);
@@ -39,7 +71,7 @@ public class Credito extends Tarjeta {
 	}
 
 	public double getSaldo() {
-		double r = 0.0;
+		double r = contImporte;
 		for (int i = 0; i < this.mMovimientos.size(); i++) {
 			Movimiento m = mMovimientos.get(i);
 			r += m.getmImporte();
@@ -51,10 +83,15 @@ public class Credito extends Tarjeta {
 		return mCredito - getSaldo();
 	}
 
+	/**
+	 * Metodo liquida las operaciones de la tarjeta de credito 
+	 * @param mes Mes
+	 * @param anyo A�o
+	 */
 	public void liquidar(int mes, int anyo) {
 		Movimiento liq = new Movimiento();
 		liq.setmConcepto("Liquidación de operaciones tarj. crédito, " + (mes + 1) + " de " + (anyo + 1900));
-		double r = 0.0;
+		double r = contImporte;
 		for (int i = 0; i < this.mMovimientos.size(); i++) {
 			Movimiento m = mMovimientos.get(i);
 			if (m.getmFecha().getMonth().getValue() + 1 == mes && m.getmFecha().getYear() + 1900 == anyo)
